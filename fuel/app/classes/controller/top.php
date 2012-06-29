54ed87c5537169ac605171c7795f9cb0f35a2c99
Yu Totsuka <yu.totsuka@givery.co.jp>
2012-06-29 20:30:39 +0900

    add twitter bootstrap

---
+++ fuel/app/classes/controller/top.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * The Top Controller.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Top extends Controller_Application
+{
+
+	/**
+	 * The basic welcome message
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		return Response::forge(View::forge('welcome/index'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(ViewModel::forge('welcome/404'), 404);
+	}
+}
